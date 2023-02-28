fec7a42fb1fb8c075b503af2d11ef8c4a19b81b7
yufongpeng <54415349+yufongpeng@users.noreply.github.com>
2023-02-28 23:44:30 +0800

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,14 @@
 using BloodVolumePCIIS, Test
 
+function test_show(x)
+    try 
+        show(IOBuffer(), x)
+        true
+    catch
+        false
+    end
+end
+
 const file_directory = joinpath(@__DIR__, "data")	# default: current directory
 const calibration_keywords = nothing # default: ["volume", "Volume"]
 const sample_keywords = [r"B\d+"] # default: ["sample", "Sample"] 
@@ -14,8 +23,12 @@ const sample_keywords = [r"B\d+"] # default: ["sample", "Sample"]
     @test last(cal.data[end].t) < 0.5
     @test extrema(cal.x) == (5, 40)
     model = optimize(cal)
+    @test test_show(model)
     et = error_table(model)
     @test extrema(et.volume) == (5, 40)
+    @test isapprox(model_volume(model), transform_volume(model).(volume(model)))
+    @test isapprox(model_response(model), transform_response(model).(response(model)))
+    @test isapprox(err(model) ./ volume(model), pcterr(model))
     cit = volume(model, cal)
     @test isapprox(unique(average_sample(cit).volume), et.predicted_volume)
 end
